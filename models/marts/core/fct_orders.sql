0e0cce95aa7ee44f86e9710862b2c77f45bea446
Greg Vaslowski <greg+dbt@vaslo.com>
2022-06-29 00:55:59 +0000

    Appear to have a working fct_orders model

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,5 +1,48 @@
 with payment as (
      select * from {{ ref('stg_payment')}}
+),
+
+orders as (
+
+    select * from {{ ref('stg_orders') }}
+
+),
+
+customer_orders as (
+
+    select 
+        order_id,
+        customer_id
+    
+    from orders
+
+
+),
+
+customer_payments as (
+
+    select 
+        order_id,
+        amount
+
+    from payment
+),
+
+final as (
+
+    select 
+        customer_orders.order_id,
+        customer_orders.customer_id,
+        sum(amount) as amount
+    
+    from
+        customer_orders
+
+    left join customer_payments using (order_id)
+
+    group by 
+        customer_orders.order_id,
+        customer_orders.customer_id
 )
 
-select * from payment
+select * from final
